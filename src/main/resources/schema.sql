e0b63d5ec0cc54633956932ff29e0dea8682cbf9
sg-1229 <hoge@gmail.com>
2020-07-13 02:22:28 +0900

    36

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,13 +7,3 @@ CREATE TABLE inquiry
    created DATETIME NOT NULL,
    PRIMARY KEY(id)
 );
-
-CREATE TABLE survey
-(
-   id INT NOT NULL AUTO_INCREMENT,
-   age INT NOT NULL,
-   satisfaction INT NOT NULL,
-   comment VARCHAR(100),
-   created DATETIME NOT NULL,
-   PRIMARY KEY(id)
-);
